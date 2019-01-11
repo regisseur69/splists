2aa39c31f1fa6887fcdc480263f93b2b6fe31977
= <=>
2019-01-11 12:08:27 +0100

    Gestion des partials

--- index.php
+++ index.php
@@ -1,23 +1,11 @@
-- index.php
+<?php include('partials/header.php'); ?>
 
-- views/
-       login.php
-       signup.php
-       board.php
+  <!--contenu de index
 
-- controllers/
-        loginControllers.php
-        signupControllers.php
-        boardControllers.php
-        helper.php
+<?php include('partials/footer.php'); ?>
 
-- partials/
-        _header.php
-        _footer.php
-        _form_ticket.php
-        _view_ticket.php
 
-- assets/
-        js/
-        css/
-        img/
+
+
+
+  
---
+++ views/signup.php
@@ -0,0 +1,11 @@
+<?php include('../partials/_header.php'); ?>
+
+  <!--contenu de index
+
+<?php include('../partials/_footer.php'); ?>
+
+
+
+
+
+  
