464b1c2a31ee676d0cf484d558df9839d318a4f4
= <=>
2019-01-11 11:31:02 +0100

    Creating files

---
+++ index.php
@@ -0,0 +1,23 @@
+- index.php
+
+- views/
+       login.php
+       signup.php
+       board.php
+
+- controllers/
+        loginControllers.php
+        signupControllers.php
+        boardControllers.php
+        helper.php
+
+- partials/
+        _header.php
+        _footer.php
+        _form_ticket.php
+        _view_ticket.php
+
+- assets/
+        js/
+        css/
+        img/
